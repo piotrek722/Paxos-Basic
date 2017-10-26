f1fb222f5c2f51c5bf2f6eb1f099ed56d0648078
Piotr Martyna <piotrek722@gmail.com>
2017-10-26 17:53:42 +0200

    Initial project structure with travis CI

---
+++ paxos/src/main/java/iosr/paxos/Main.java
@@ -0,0 +1,16 @@
+package iosr.paxos;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Check");
+    }
+
+    /**
+     * Sample method to test.
+     */
+    public String getText(){
+        return "Check2";
+    }
+
+}
---
+++ paxos/src/test/java/iosr/paxos/MainTest.java
@@ -0,0 +1,16 @@
+package iosr.paxos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    private Main main = new Main();
+
+    @Test
+    public void getText() throws Exception {
+        assertEquals("Check1", main.getText());
+    }
+
+}
