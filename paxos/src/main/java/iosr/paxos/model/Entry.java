f3dacb5815576f2f207e740d62ea382a9ca8ab15
Piotr Martyna <piotrek722@gmail.com>
2017-11-04 23:58:14 +0100

    Entry equals override

--- paxos/src/main/java/iosr/paxos/model/Entry.java
+++ paxos/src/main/java/iosr/paxos/model/Entry.java
@@ -23,4 +23,22 @@ public final class Entry implements Serializable{
     public String getValue() {
         return value;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Entry entry = (Entry) o;
+
+        if (key != null ? !key.equals(entry.key) : entry.key != null) return false;
+        return value != null ? value.equals(entry.value) : entry.value == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = key != null ? key.hashCode() : 0;
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        return result;
+    }
 }
