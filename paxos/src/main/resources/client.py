9ad09d8ec600b24c27b12f828e0800c9733ca6f8
leeshlay <kijak.shia@gmail.com>
2017-11-08 23:39:14 +0100

    Add client script

---
+++ paxos/src/main/resources/client.py
@@ -0,0 +1,44 @@
+import requests
+import random
+
+servers = ['https://paxos-1.herokuapp.com',
+           'https://paxos-2.herokuapp.com',
+           'https://paxos-3.herokuapp.com',
+           'https://paxos-4.herokuapp.com',
+           'https://paxos-5.herokuapp.com']
+
+
+def check_values(key):
+    print('Checking values...')
+    for server in servers:
+        r = requests.get(server + '/listener/' + key)
+        print(server, r.status_code)
+
+
+print('Sending one propose message')
+
+key = 'test'
+value = '2'
+
+server = random.choice(servers)
+r = requests.post(server + '/proposer/propose?key=' + key + '&value=' + value)
+print(server, r.status_code)
+check_values(key)
+
+
+print('Sending propose messages with different values')
+
+key = 'test2'
+value1 = '2'
+value2 = '4'
+
+server1 = random.choice(servers)
+server2 = random.choice(servers)
+
+r1 = requests.post(server1 + '/proposer/propose?key=' + key + '&value=' + value1)
+r2 = requests.post(server2 + '/proposer/propose?key=' + key + '&value=' + value2)
+
+print(server1, r1.status_code)
+print(server2, r2.status_code)
+
+check_values(key)
