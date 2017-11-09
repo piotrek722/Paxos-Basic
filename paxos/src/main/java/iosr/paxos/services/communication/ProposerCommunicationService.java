d728c603461c728834dc257e2df6b22b5f6db588
leeshlay <kijak.shia@gmail.com>
2017-11-09 21:53:44 +0100

    Handle service unavailable error

--- paxos/src/main/java/iosr/paxos/services/communication/ProposerCommunicationService.java
+++ paxos/src/main/java/iosr/paxos/services/communication/ProposerCommunicationService.java
@@ -3,9 +3,12 @@ package iosr.paxos.services.communication;
 import iosr.paxos.model.Data;
 import iosr.paxos.model.SequenceNumber;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,9 +20,17 @@ public class ProposerCommunicationService extends CommunicationService {
     }
 
     public List<Data> sendPromiseToAll(SequenceNumber sequenceNumber) {
-        return getServersUrls().stream().map(serverUrl ->
-                restTemplate.postForEntity(serverUrl + "/acceptor/proposal?sequenceNumber", sequenceNumber, Data.class).getBody()
-        ).collect(Collectors.toList());
+
+       return getServersUrls().stream().map(serverUrl -> {
+           try {
+               return restTemplate
+                               .postForEntity(serverUrl + "/acceptor/proposal?sequenceNumber", sequenceNumber, Data.class)
+                               .getBody();
+           } catch (HttpStatusCodeException exception) {
+               return null;
+           }
+       })
+               .filter(Objects::nonNull).collect(Collectors.toList());
     }
 
     public void sendAcceptToAll(Data data) {
